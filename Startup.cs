4852f9419fc7445f59ab9290ad595f5c2fa895af
Ranu <O@O.net>
2017-02-01 13:16:06 +0000

    Add Static File Support via configuring Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,9 @@
+using Microsoft.AspNetCore.Builder;
 public class Startup
 {
-    public void Configure() { }
+    public void Configure(IApplicationBuilder app)
+    {
+        app.UseDefaultFiles();
+        app.UseStaticFiles();
+    }
 }
