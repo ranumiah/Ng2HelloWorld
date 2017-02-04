360912ebd5297d8001989190c04ced6d9104b500
Ranu <ranu.miah@outlook.com>
2017-02-04 12:36:11 +0000

    Add Angular bootstrap to index.html

--- app/main.ts
+++ app/main.ts
@@ -1,1 +1,8 @@
-document.body.innerHTML = 'Hello, TypeScript World!';
+//document.body.innerHTML = 'Hello, TypeScript World!';
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app.module';
+
+// This is using (JIT) compilation in the browser with platformBrowserDynamic to bootstrap the AppModule.
+platformBrowserDynamic().bootstrapModule(AppModule);
