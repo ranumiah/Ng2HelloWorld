065e3e3e68be47099b6a9b746ed45bd1ab2ac46e
Ranu <O@O.net>
2017-02-08 11:05:20 +0000

    Refactor to Multiple Components without Routing enabled

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppComponent } from './app.component';
+import { PairingComponent } from './pairing.component';
+import { AboutComponent } from './about.component';
 
 @NgModule({
-    bootstrap: [ AppComponent ],
-    imports: [ BrowserModule ],
-    declarations: [ AppComponent ],
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+    ],
+    declarations: [
+        AppComponent,
+        PairingComponent,
+        AboutComponent,
+    ],
 })
 export class AppModule { }
 
